4f4c1ffbb5a53afdf34471b705074e6842845952
BHamp <brianscotthampton@gmail.com>
2020-07-28 19:20:15 -0400

    Setup for new hosted instances

--- server/index.js
+++ server/index.js
@@ -8,22 +8,22 @@ const port = 2112;
 app.use(express.static(path.join(__dirname, '../public')));
 
 const mediaProxy = createProxyMiddleware({
-  target: 'http://3.23.132.230:8000',
+  target: 'http://18.219.209.226:8000/',
   changeOrigin: true,
 });
 
 const reviewProxy = createProxyMiddleware({
-  target: 'http://54.67.101.150:4200',
+  target: 'http://18.218.201.173:4200/',
   changeOrigin: true,
 });
 
 const announcementProxy = createProxyMiddleware({
-  target: 'http://3.17.138.9:8080',
+  target: 'http://18.217.243.249:8080/',
   changeOrigin: true,
 });
 
 const bodyProxy = createProxyMiddleware({
-  target: 'http://52.14.114.30:1992',
+  target: 'http://18.191.250.26:1992/',
   changeOrigin: true,
 });
 
